eb37ed6f9ea88090447777c27b167c5e542b4106
Remo Wettstein <remo1995@bluewin.ch>
2025-06-27 10:19:19 +0200

    adding whole db stuff

---
+++ DB/mongoshell.sh
@@ -0,0 +1,1 @@
+docker exec -it mongo-shell mongosh "mongodb://root:example@mongo:27017"
---
+++ DB/run.sh
@@ -0,0 +1,3 @@
+docker compose -f mongo/docker-compose.mongo.yaml up -d
+docker compose -f mongo-express/docker-compose.express.yaml up -d
+docker compose -f mongo-shell/docker-compose.shell.yaml up -d
