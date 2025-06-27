ed7ea8ef195e1f5fdb88e6c45d9ed0d03104ed40
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2025-06-27 09:05:18 +0200

    initial commit

---
+++ backend/m165-kochbuch-backend/src/main/java/ch/wiss/m165kochbuchbackend/M165KochbuchBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.m165kochbuchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M165KochbuchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M165KochbuchBackendApplication.class, args);
+    }
+
+}
---
+++ backend/m165-kochbuch-backend/src/test/java/ch/wiss/m165kochbuchbackend/M165KochbuchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.m165kochbuchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M165KochbuchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
