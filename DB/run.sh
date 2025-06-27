59f3c0c90d758c4f6d45100a7851778c145660ee
Remo Wettstein <remo1995@bluewin.ch>
2025-06-27 12:02:39 +0200

    changed run.sh file

--- DB/run.sh
+++ DB/run.sh
@@ -1,3 +1,6 @@
-docker compose -f mongo/docker-compose.mongo.yaml up -d
+#!/bin/bash
+docker network inspect mongo-net >/dev/null 2>&1 || docker network create -d bridge mongo-net
+docker compose -f mongodb/docker-compose.mongo.yaml up -d
 docker compose -f mongo-express/docker-compose.express.yaml up -d
 docker compose -f mongo-shell/docker-compose.shell.yaml up -d
+
