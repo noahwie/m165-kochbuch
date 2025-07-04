f0ea77341e372f6f14403770f97290388755b7ff
silas <silas@enzler.info>
2025-07-04 13:44:24 +0200

    unnötige imports geläscht

--- backend/m165-kochbuch-backend/src/main/java/ch/wiss/m165kochbuchbackend/repository/RezeptRepository.java
+++ backend/m165-kochbuch-backend/src/main/java/ch/wiss/m165kochbuchbackend/repository/RezeptRepository.java
@@ -1,7 +1,6 @@
 package ch.wiss.m165kochbuchbackend.repository;
 
 import ch.wiss.m165kochbuchbackend.model.Rezept;
-import jdk.jfr.Category;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
