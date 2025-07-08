f8cd41e690802818ba05a6caae70056d50cf97df
Remo Wettstein <remo1995@bluewin.ch>
2025-07-08 08:39:27 +0200

    additional Testing

---
+++ backend/m165-kochbuch-backend/src/test/java/ch/wiss/m165kochbuchbackend/smoke/CookBookApplicationSmokeTest.java
@@ -0,0 +1,15 @@
+package ch.wiss.m165kochbuchbackend.smoke;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookBookApplicationSmokeTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
